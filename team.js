ea5a1c90a48347acfc88b3aa8fb88113a60b25a7
shmakovdd <60802799+shmakovdd@users.noreply.github.com>
2020-03-27 15:14:14 +0700

    Add files via upload

---
+++ team.js
@@ -0,0 +1,34 @@
+function accordeonTeam() {
+
+    const workers = document.querySelectorAll('.team__item');
+    const accordeon = document.querySelector('.team__accordeon');
+
+    accordeon.addEventListener ('click', function(event) {
+
+        const target = event.target;
+
+        if(target.classList.contains('.team__name')) {
+            const worker = target.querySelector('.team__name');
+            const content = target.nextElementSibling;
+            const contentHeight = content.firstElementChild.clientHeight;
+                
+            for (iterator of workers) {
+                if (iterator !== worker) {
+                    iterator.classList.remove('.team__item--active');
+                    iterator.lastElementChild.style.height = 0;
+                }
+            }
+
+            if (worker.classList.contains('.team__item--active')) {
+                worker.classList.remove('.team__item--active');
+                content.style.height = 0;
+                } else {
+                    worker.classList.add('.team__item--active');
+                    content.style.height = contentHeight + "px";
+                }
+        }        
+    
+}) 
+}
+
+accordeonTeam();
